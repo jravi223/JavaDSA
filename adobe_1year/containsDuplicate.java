029bb99c0310168ae2dd88f9a6d74075c723c836
Ravi J <jravi223@gmail.com>
2025-07-07 19:35:13 +0530

    added few left

--- adobe_1year/containsDuplicate.java
+++ adobe_1year/containsDuplicate.java
@@ -1,21 +1,22 @@
 package adobe_1year;
 
+import java.security.PublicKey;
 import java.util.HashSet;
 import java.util.Set;
 
 public class containsDuplicate {
     public static void main(String[] args) {
-    System.out.println("ijhiojio");
-     public boolean containsDuplicates(int[] nums) {
-
-    Set<Integer> seen = new HashSet<>();
-
-        for (final int num : nums)
-            if (!seen.add(num))
-                return true;
-
-        return false;
-    }
+//    System.out.println("ijhiojio");
+//      Boolean containsDuplicates(int[] nums) {
+//
+//    Set<Integer> seen = new HashSet<>();
+//
+//        for (final int num : nums)
+//            if (!seen.add(num))
+//                return true;
+//
+//        return false;
+//    }
 }
 
 }
