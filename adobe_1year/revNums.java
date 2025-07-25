eeb8ce6d7dcbab59cac6b12b06b230f8fe6dbddc
Ravi J <jravi223@gmail.com>
2025-07-26 01:01:28 +0530

    hit

---
+++ adobe_1year/revNums.java
@@ -0,0 +1,11 @@
+package adobe_1year;
+
+public class revNums {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 2, 3 };
+
+        for (int i = nums.length - 1; i <= 0; i--) {
+            System.out.println("rev arr " );
+        }
+    }
+}
